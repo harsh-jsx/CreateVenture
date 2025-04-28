c9eefe2e8f630c7d3d80f791c6b76fb0795ef50b
maddemon798 <hardiktaneja838@gmail.com>
2025-04-28 19:13:14 +0530

    added react-router-dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,19 @@
-function App() {
-  console.log(theme);
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
+const App = () => {
   return (
-    <div data-theme={theme}>
-      <h1 className="text-3xl font-bold underline">lawda man</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
