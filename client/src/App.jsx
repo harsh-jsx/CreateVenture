a0117a277608dcf3e1ec553839aec6a0281eac14
Harsh <harshh.tsx@gmail.com>
2025-04-28 18:22:44 +0530

    Initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,42 @@
+import { useStore } from "zustand";
+import { useThemeStore } from "./stores/ThemeAuthStore.js";
+import { THEMES } from "./constants/index.js";
+
+function App() {
+  const { theme, setTheme } = useThemeStore();
+  console.log(theme);
+
+  return (
+    <div data-theme={theme}>
+      <h1 className="text-3xl font-bold underline">lawda man</h1>
+
+      {THEMES.map((t) => (
+        <button
+          key={t}
+          className={`
+                group flex flex-col items-center gap-1.5 p-2 rounded-lg transition-colors
+                ${theme === t ? "bg-base-200" : "hover:bg-base-200/50"}
+              `}
+          onClick={() => setTheme(t)}
+        >
+          <div
+            className="relative h-8 w-full rounded-md overflow-hidden"
+            data-theme={t}
+          >
+            <div className="absolute inset-0 grid grid-cols-4 gap-px p-1">
+              <div className="rounded bg-primary"></div>
+              <div className="rounded bg-secondary"></div>
+              <div className="rounded bg-accent"></div>
+              <div className="rounded bg-neutral"></div>
+            </div>
+          </div>
+          <span className="text-[11px] font-medium truncate w-full text-center">
+            {t.charAt(0).toUpperCase() + t.slice(1)}
+          </span>
+        </button>
+      ))}
+    </div>
+  );
+}
+
+export default App;
---
+++ client/tailwind.config.js
@@ -0,0 +1,44 @@
+import { defineConfig } from "vite";
+import daisyui from "daisyui";
+
+import tailwindcss from "@tailwindcss/vite";
+export default defineConfig({
+  plugins: [tailwindcss(), daisyui],
+  // thenes
+  daisyui: {
+    themes: [
+      "light",
+      "dark",
+      "cupcake",
+      "bumblebee",
+      "emerald",
+      "corporate",
+      "synthwave",
+      "retro",
+      "cyberpunk",
+      "valentine",
+      "halloween",
+      "garden",
+      "forest",
+      "aqua",
+      "lofi",
+      "pastel",
+      "fantasy",
+      "wireframe",
+      "black",
+      "luxury",
+      "dracula",
+      "cmyk",
+      "autumn",
+      "business",
+      "acid",
+      "lemonade",
+      "night",
+      "coffee",
+      "winter",
+      "dim",
+      "nord",
+      "sunset",
+    ],
+  },
+});
